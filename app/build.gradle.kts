acb2aa70bfd24eb654e76bf2d5541b47fc7f53f3
Sarfaraj Biswas <ab.sarfaraj@gmail.com>
2024-12-20 20:03:30 +0530

    remove theme fromlibrary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,6 @@ dependencies {
     implementation(libs.gson)
 
     ///libs
-    implementation( "com.github.sarfarajbiswas:DeemaSDKV1:1.0")
-    //implementation(project(":deemasdkv1"))
+//    implementation( "com.github.sarfarajbiswas:DeemaSDKV1:1.0")
+    implementation(project(":deemasdkv1"))
 }
